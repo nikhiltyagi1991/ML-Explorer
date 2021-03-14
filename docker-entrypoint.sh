6da19f6101f0f7f1730f47c22255fd850c61220f
Nik <ntyagi@kanetix.ca>
2021-03-14 17:54:36 -0400

    Prod env with waitress added

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+if test "$FLASK_ENV" = "prod"; then
+    echo "Starting prod server"
+    waitress-serve --call 'base:create_app'
+else
+    echo "Starting dev server"
+    flask run --host=0.0.0.0 --port=5000
+fi
